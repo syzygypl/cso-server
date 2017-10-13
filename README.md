186691f38781d4d2b42a343e79b7f3fceb956e6e
bsk <bartlomiej.glowala@syzygy.pl>
2017-10-13 13:42:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSO-server
+
+Simple server dedicated for running CSO on heroku. Bootstraps backend and client apps. Solves communication between them.
+
+Requires following buildpacks:
+1. https://github.com/timshadel/heroku-buildpack-github-netrc.git
+2. https://github.com/heroku/heroku-buildpack-nodejs#v83
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const proxy = require('http-proxy-middleware');
+const basicAuth = require('express-basic-auth');
+
+const apiProxy = proxy(
+  '/api',
+  {
+    target: 'http://localhost:' + process.env.BACKEND_PORT,
+    pathRewrite: { '^/api' : '' }
+  }
+);
+
+app.use(basicAuth({
+  users: {
+    [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASS},
+  challenge: true,
+  realm: 'csoJa4ds92a2ad12w1'
+}));
+
+app.use(apiProxy);
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile('./build/index.html', { root: __dirname });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+});
